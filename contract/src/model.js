090498f6c7ec5886c3f47ed81d4831a5cca27d33
Carlos Juarez <carlos.juarez9141@alumnos.udg.mx>
2022-11-26 19:13:49 -0600

    Desarrollo terminado

---
+++ contract/src/model.js
@@ -0,0 +1,6 @@
+export const STORAGE_COST = BigInt("1000000000000000000000")
+
+export class Donation {
+  account_id;
+  total_amount;
+}
---
+++ frontend/origen.js
@@ -0,0 +1,3 @@
+import { NearBindgen, near, call, view, initialize, UnorderedMap } from 'near-sdk-js'
+import { assert } from './utils'
+
